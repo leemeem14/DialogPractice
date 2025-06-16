ba94a838110febb21dd8f3a8aa81d3e6b3df83f1
leemeem14 <leemeem19@naver.com>
2025-06-16 14:54:31 +0900

    대화상자 항목(item)표시

--- app/src/main/java/kr/ac/kopo/dialogpractice/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/dialogpractice/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,25 +28,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btn1 = findViewById(R.id.btn1);
+        ImageView fimg = findViewById(R.id.fimg);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] fooditems = {"냉면", "물냉면", "비빔냉면"};
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                dlg.setTitle("bruh");
-                dlg.setMessage("wat is dis shiii");
+                dlg.setTitle("니가 원하는거");
+                dlg.setItems(fooditems, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        btn1.setText(fooditems[which]);
+                        if (which == 0) {
+                            fimg.setImageResource(R.drawable.cn);
+                        } else if (which == 1) {
+                            fimg.setImageResource(R.drawable.wcn);
+                        } else {
+                            fimg.setImageResource(R.drawable.bcn);
+                        }
+                    }
+                });
+//                dlg.setMessage("wat is dis shiii");
                 dlg.setIcon(R.mipmap.ic_launcher);
-                dlg.setPositiveButton("확 인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this, "gud", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                dlg.setNegativeButton("취 소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this, "nope", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                dlg.setPositiveButton("닫 기", null);
+//                dlg.setNegativeButton("취 소", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MainActivity.this, "nope", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
                 dlg.show();
             }
         });
